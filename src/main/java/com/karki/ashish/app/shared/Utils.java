c85c22b744b70528439b924d0e5a770890cc7ff7
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-09-08 12:13:10 +0545

    Creating Utils file with random alphanumeric generator logic.

--- src/main/java/com/karki/ashish/app/shared/Utils.java
+++ src/main/java/com/karki/ashish/app/shared/Utils.java
@@ -10,6 +10,20 @@ public class Utils {
 
 	private final Random RANDOM = new SecureRandom();
 	private final String ALPHANUMERIC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWNYZabcdefghijklmnopqrstuvwnyz";
-	private final int iterations = 10000;
-	private final int KEY_LENGTH = 256;
+//	private final int iterations = 10000;
+//	private final int KEY_LENGTH = 256;
+
+	public String generateUserId(int length) {
+		return generateRandomString(length);
+	}
+
+	private String generateRandomString(int length) {
+		StringBuilder returnValue = new StringBuilder(length);
+
+		for (int i = 0; i < length; i++) {
+			returnValue.append(ALPHANUMERIC.charAt(RANDOM.nextInt(ALPHANUMERIC.length())));
+		}
+
+		return new String(returnValue);
+	}
 }
